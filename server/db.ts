df0cdacf55ed2a5ff73ef38c73d28ac45eebc728
AayushY02 <aayush.yadav@tk.pacific.co.jp>
2025-06-12 13:45:39 +0900

    Onboarding merge test (#2)
    * testing again
    
    * fix - local build env errors

--- server/db.ts
+++ server/db.ts
@@ -1,8 +1,9 @@
+import dotenv from "dotenv";
+dotenv.config();
 import { Pool, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
 import ws from "ws";
 import * as schema from "@shared/schema";
-
 neonConfig.webSocketConstructor = ws;
 
 if (!process.env.DATABASE_URL) {
